b64480946f88d936bd3b0277924df68cba35d5b1
Chase Sunstrom <122314695+ChaseSunstrom@users.noreply.github.com>
2023-05-18 21:14:14 -0500

    Fixed unwraped and unparsed expression value.

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,18 @@ fn main() {
     println!("Welcome to the calculator! Make sure to enter a valid mathematical operation. And add a space after each character/number.");
 
     loop{
-        println!("Enter a mathematical operation.");
+        println!("Enter a mathematical operation:");
         let mut input = String::new();
         io::stdin().read_line(&mut input).expect("Failed to read line");
-        let mut result = eval(&input);
-        println!("{:?}" , result);
+        let result = eval(&input).unwrap();
+
+        let extracted_result = match result {
+            Value::Int(x) => x,
+            Value::Float(x) => x.round() as i64,
+            _ => panic!("Error parsing value.")
+        };
+
+        println!("{:?}" , extracted_result);
 
         println!("Break? (y/n)");
         let mut question = String::new();
